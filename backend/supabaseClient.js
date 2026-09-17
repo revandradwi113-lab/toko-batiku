e0702df07e5e08233fa035e456860abf865f4561
revandradwi113-lab <revandra.dwi113@smk.belajar.id>
2026-09-17 13:33:36 +0700

    fix nama env variable supabase service key

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -6,7 +6,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_KEY // JANGAN pakai anon key di sini
+  process.env.SUPABASE_SERVICE_ROLE_KEY // JANGAN pakai anon key di sini
 );
 
 module.exports = supabase;
